e9c84921f00ba430aa654645c06624c6c94182e3
Jencz <2180867@johnabbottcollege.net>
2024-09-18 14:13:08 -0400

    m

--- SamuraiApp.Domain/SamuraiApp.UI/Program.cs
+++ SamuraiApp.Domain/SamuraiApp.UI/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.UI
+{
+    internal class Program
+    {
+        private static SamuraiContext _context = new SamuraiContext();
+        static void Main(string[] args)
+        {
+            _context.Database.EnsureCreated();
+            GetSamurai("Before Add:");
+            AddSamurais("Claudiu Sensei", "Aref Sensei", "Sandy Sensei", "Meghrig Sensei", "Gabriel Sensei", "Larry Sensei", "Ian Sensei", "Helen Sensei", "Vik Sensei", "Talib Sensei");
+            GetSamurai("After Add:");
+        }
+        private static void GetSamurai(string msg)
+        {
+            var samurais = _context.Samurais.ToList();
+            Console.WriteLine($"{msg} Samurai count is {samurais.Count}");
+
+            if (samurais.Count > 0)
+            {
+                foreach (var samurai in samurais)
+                {
+                    Console.WriteLine(samurai.Name);
+                }
+            }
+            else
+                Console.WriteLine("No samurais.");
+        }
+        private static void AddSamurais(params string[] names)
+        {
+            foreach (string name in names)
+            {
+                _context.Samurais.Add(new Samurai { Name = name });
+            }
+            _context.SaveChanges();
+        }
+    } 
+}
